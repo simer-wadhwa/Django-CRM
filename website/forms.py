deefbfaca68af435adea0885d5757752cad147cb
Simer <simerwadhwa115@gmail.com>
2023-06-24 11:39:45 -0700

    Database Model

--- website/forms.py
+++ website/forms.py
@@ -32,3 +32,8 @@ class SignUpForm(UserCreationForm):
             self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
 
 
+
+
+
+
+
