0893dc6223c0c2274832474f59d02ada479173cf
Simer <simerwadhwa115@gmail.com>
2023-06-24 21:57:47 -0700

    Update the Record

--- website/views.py
+++ website/views.py
@@ -91,6 +91,23 @@ def add_record(request):
 
     return render(request, 'add_record.html', {'form': form})
 
+def update_record(request,pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record has been updated!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, "You must be Login...")
+        return redirect('home')
+
+
+
+
+
 
 
 
