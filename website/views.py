ea0c51d78224f0b83f6eb5cf97b2cdaf2b136bf6
Simer <simerwadhwa115@gmail.com>
2023-06-24 17:54:39 -0700

    Delete the Record

--- website/views.py
+++ website/views.py
@@ -64,6 +64,18 @@ def customer_record(request, pk):
         messages.success(request, "You Must be Login to view the page!")
         return redirect('home')
 
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Deleted Successfully...")
+        return redirect('home')
+    else:
+        messages.success(request, "You Must be Login to Delete the Record!")
+        return redirect('home')
+
+
+
 
 
 
